5710ffff54d84082681ee71439f9c18982189b9e
JohnNjenga <jnjenga@chskenya.org>
2014-04-25 11:36:13 +0300

    My first push

---
+++ assignment 2.R
@@ -0,0 +1,8 @@
+
+pollutantmean<-function(directory,pollutant,id=1:332){
+	directory<-getwd()
+		pollutant<-data$nitrate/data$sulfate
+			data <- do.call("rbind", lapply( list.files("d:/CHS/TB Program/Ref/R Programming/specdata),
+				read.csv, header=TRUE))
+	mean(pollutant)
+}
---
+++ specdata/pollutant.R
@@ -0,0 +1,9 @@
+
+pollutantmean<-function(directory,pollutant,id=1:332){
+	directory<-getwd()
+		pollutant<-data$nitrate
+		id=data$ID
+			data <- do.call("rbind", lapply(list.files("directory"),
+				read.csv, header=TRUE))
+	mean(pollutant, na.rm=TRUE)
+}
